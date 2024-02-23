7b65dcaad5f451957eb5855a99cfe072c42a3f5f
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-02-22 22:36:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,24 @@
 import pygame
+import test
+
 pygame.init()
 
-white, black, red = (255, 255, 255), (0, 0, 0), (255, 0, 0)
-height, width = 800, 600
-gameDisplay = pygame.display.set_mode((height, width))   
-squareSize = (height/8, width/8)
+piece = test.Piece(None, None, None, None, None)
+board = test.Board((None, None, None, None))
+whitepawn = test.Pawn('white', 1 , 1)
+screen = test.screen
 
+running = True
+while(running):
+    board.drawBoard()
 
-pygame.display.set_caption('chess')
-gameDisplay.fill(white)
-
-def setBoard(gameDisplay, squareSize, white, black):
-    for row in range(8):
-        for column in range(8):
-            color =  black if(row + column) % 2 == 0 else white
-            index = (row, column)
-            pygame.draw.rect(gameDisplay, color, [row*squareSize[0], column*squareSize[1], squareSize[0], squareSize[1]])
-    
-    #format the grid
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+    board.drawBoard()   
+    whitepawn.draw(screen)
+    pygame.display.update()
+        
         
 
-    
-    
-def pawn():
-    d
-    
-    
-    
-
-
-def main(gameDisplay, setBoard):
-    running = True
-    while(running):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        setBoard(gameDisplay, squareSize, white, black)
-        pygame.display.flip()
-        
-    pygame.quit()
-    
-main(gameDisplay, setBoard)
