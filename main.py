385a0144137283a3cb51c61393fee8bc725acd22
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-02-26 01:16:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,20 @@ import functions
 pygame.init()
 
 
-white, black, red = (255, 255, 255), (0, 0, 0), (255, 0, 0)
 Board = functions.Board()
+player1 = functions.player('white')
+player2 = functions.player('black')
 
 
 running = True
 while(running):
     Board.drawBoard()
-    for i in range(8):
-        for i in range(8):
-            pawn_white = functions.Pawn(1, i, white)
-            pawn_white.draw(1, i, white)
-            Board.chessBoard[1][i] = pawn_white
-
-            pawn_black = functions.Pawn(6, i, black)
-            pawn_black.draw(6, i, black)
-            Board.chessBoard[6][i] = pawn_black
+    player1.draw()
+    player2.draw()
     for event in pygame.event.get():
-        
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
     pygame.display.update()
 pygame.quit()
+    
