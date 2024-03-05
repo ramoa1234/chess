2925cb8a6b0cea49ece8f644bcf53c2713e9a5dc
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-03-05 01:51:13 -0500

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,57 +1,88 @@
 import pygame
+
 pygame.init()
 
-pygame.display.set_caption('chess')
+whitePawn = pygame.image.load('images/whitepawn.png')
+blackPawn = pygame.image.load('images/blackpawn.png')
+whiteRook = pygame.image.load('images/whiterook.png')
+blackRook = pygame.image.load('images/blackrook.png')
+whiteKnight = pygame.image.load('images/whitehorse.png')
+blackKnight = pygame.image.load('images/chess.png')
+whiteBishop = pygame.image.load('images/whiterook.png')
+blackBishop = pygame.image.load('images/blackrook.png')
+whiteQueen = pygame.image.load('images/whitequeen.png')
+blackQueen = pygame.image.load('images/blackqueen.png')
+whiteKing = pygame.image.load('images/whiteking.png')
+blackKing = pygame.image.load('images/blackking.png')
+
+pieces = [whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn,
+            whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop, whiteKnight, whiteRook,
+            blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn,
+            blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook]
+
+whitePieces=[whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn,
+            whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop, whiteKnight, whiteRook]
+whiteLocations=[(0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1),
+                (0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
+blackPieces=[blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn,
+            blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook]
+blackLocations=[(0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6),
+                (0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7)]
+
 width, height = 800, 800
-white, black = (255, 255, 255), (0, 0, 0)
 screen = pygame.display.set_mode((width, height))
+white, black, grey = (255, 255, 255), (0, 0, 0), (36, 36, 36)
 screen.fill(white)
-squareSize = (width/8, height/8)
+squareSize = (100, 100)
 
-blackPawn = pygame.image.load('blackpawn.png')
+def drawBoard():
+    for x in range(8):
+        for y in range(8):
+            color = white if (x + y) % 2 == 0 else grey
+            pygame.draw.rect(screen, color, (x * squareSize[0], y * squareSize[1], squareSize[0], squareSize[1]))
 
 
-chessBoard  = [['' for _ in range(8)] for  _ in range(8)]
-#@chessBoard[2][1] = 
+def drawPieces(whitePieces, whiteLocations, blackPieces, blackLocations, pieces):
+    mouseClick = pygame.mouse.get_pressed()
+    turnStep = 0
+
+    for i in range(len(whitePieces)):
+        piece = whitePieces[i]
+        location = whiteLocations[i]
+        screen.blit(piece, (location[0] * 100 + 22, location[1] * 100 + 30))
+#update turnStep steps need to be corrected
+#and need to figure out how to use i and j vairable in the turn steps
+    while(turnStep >= 1):
+        if(mouseClick == whiteLocations):
+            #update to have whiteePiecseSelected
+            pieceSelected = pieces[mouseClick[0]][mouseClick[1]]
+            if(turnStep == 1):
+                if(mouseClick == validMove):
+                    piecesSelected = mouseClick
+                    turnStep = 2
 
 
-
-    
-
-class Piece:
-    def __init__(self, iamge, position  ):
-        self.iamge = iamge
-        self.position = position    
-        
-    def centerPiece():
+    for j in range(len(blackPieces)):
+        piece = blackPieces[j]
+        location = blackLocations[j]
+        screen.blit(piece, (location[0] * 100 + 22, location[1] * 100 + 30))
+    while(turnStep >= 2):
         pass
-    
-    def drawPiece():
-        pass
-    
-    def pieceSelected():
-        pass
-    
-    
-    
-    
-def pawn(piece):
+
+def validMove():
     pass
 
-#setboard(display)
-def drawBoard(screen, white, black, squareSize):
-    for row in range(8):
-        for column in range(8):
-            color = black if(row + column) % 2 == 0 else white
-            pygame.draw.rect(screen, color, (column * squareSize[0], row * squareSize[1], squareSize[0], squareSize[1]))
 
-def main():
-    run = True
-    while(run):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        drawBoard(screen, white, black,squareSize)
-        pygame.display.flip()
+running = True
+while running:
+    screen.fill(white)
+    drawBoard()
+    drawPieces(whitePieces, whiteLocations, blackPieces, blackLocations)
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-main()
+
+pygame.quit()
+
